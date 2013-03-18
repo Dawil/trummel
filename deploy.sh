b74aab21f50bb999077bffdd2d448c62585cbd63
dave <dave@davidgwilcox.com>
2013-03-18 12:20:15 +1100

    improving deploy.sh to quit if uncommited changes

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
 echo Running deploy script. Will merge master into gh-pages, add main.js and main.css, then push to gh-pages.
+if [ -z `git status | grep 'nothing to commit'` ]
+then
+	echo things to commit
+else
+	echo nothing to commit
+fi
+exit 0
+
 git checkout gh-pages
 git merge master
 grunt
