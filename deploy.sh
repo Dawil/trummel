bc53d85b9858a0e949a9fe2b764346d1a7f76de6
dave <dave@davidgwilcox.com>
2013-03-18 12:27:33 +1100

    testing deploy script requires git commits

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-echo Running deploy script. Will merge master into gh-pages, add main.js and main.css, then push to gh-pages.
+echo Running deploy script. Will merge master into gh-pages, add main.js and
+echo main.css and then push to gh-pages...
 
 STATUS=`git status | grep 'nothing to commit'`
-echo $STATUS
 if [ -z "$STATUS" ]
 then
-	echo things to commit
+	git status
+	exit 0
 else
-	echo nothing to commit
+	echo Nothing to commit, continuing.
 fi
-exit 0
 
 git checkout gh-pages
 git merge master
